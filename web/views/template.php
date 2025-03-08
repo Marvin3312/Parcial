03ba2a5b800495331981ca0e09d6f4ddbe714034
Marvin3312 <marvinhernandez2000@gmail.com>
2025-03-08 15:13:41 -0600

    change the structure porftolio and template

--- web/views/template.php
+++ web/views/template.php
@@ -12,15 +12,15 @@ $path = TemplateController::path(); // Obtiene la ruta base
     <meta name="author" content=""/>
     <title>Freelance - Start Bootstrap Theme</title>
     <!-- Favicons -->
-    <link rel="icon" type="image/png" href="<?php echo $path; ?>/estrcutura_correcta/web/views/assets/favicon.png" />
+    <link rel="icon" type="image/png" href="<?php echo $path; ?>/Parcial/web/views/assets/favicon.png" />
     <!-- Google fonts-->
     <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
     <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css" />
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <!-- Custom CSS -->
-    <link rel="icon" type="image/png" href="<?php echo $path; ?>/estrcutura_correcta/web/views/assets/img/favicon.png" />
-    <link rel="stylesheet" href="<?php echo $path; ?>/estrcutura_correcta/web/views/assets/css/styles.css">
+    <link rel="icon" type="image/png" href="<?php echo $path; ?>/Parcial/web/views/assets/img/favicon.png" />
+    <link rel="stylesheet" href="<?php echo $path; ?>/Parcial/web/views/assets/css/styles.css">
       
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css">
 
